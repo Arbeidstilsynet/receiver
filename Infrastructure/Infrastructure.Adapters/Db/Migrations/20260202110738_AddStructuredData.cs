be8cadb0daf37e9d37e37b85e265e641768df16e
Bjørnar Prytz <bjornar.groholt.prytz@arbeidstilsynet.no>
2026-02-02 12:19:10 +0100

    migrations + fix some tests

---
+++ Infrastructure/Infrastructure.Adapters/Db/Migrations/20260202110738_AddStructuredData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Arbeidstilsynet.MeldingerReceiver.Infrastructure.Adapters.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStructuredData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAttachment",
+                schema: "public",
+                table: "documents");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DocumentType",
+                schema: "public",
+                table: "documents",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DocumentType",
+                schema: "public",
+                table: "documents");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAttachment",
+                schema: "public",
+                table: "documents",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
